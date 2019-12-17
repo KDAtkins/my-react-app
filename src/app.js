036d4b1ae1f2539dd70d913bc0bfa3113edca8bb
Kevin Atkins <kevindewayneatkins@gmail.com>
2019-12-16 18:52:45 -0800

    completed JSX challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log("App.js is running");
 
-// JSX = Javascript XML
-var template = <h1>Does this change for real?</h1>;
+// // JSX = Javascript XML
+// var template = (
+// 	<div>
+// 		<h1>Indecision App</h1>
+// 		<p>This is some info</p>
+// 		<ol>
+// 			<li>Item one</li>
+// 			<li>Item two</li>
+// 		</ol>
+// 	</div>
+// );
+
+var myTemplate = (
+	<div>
+		<h1>Kevin Atkins</h1>
+		<p>Age: 32</p>
+		<p>Location: San Diego</p>
+	</div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(myTemplate, appRoot);

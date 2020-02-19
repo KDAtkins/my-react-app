60d70788c050e2a908949350a396dadcf21ee90c
Kevin Atkins <kevindewayneatkins@gmail.com>
2020-02-19 13:51:06 -0800

    commented out challenge code for review and to refresh

--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -12,13 +12,13 @@
 //
 // console.log(squareArrow(9));
 
-const fullName = 'Kevin D. Atkins';
-
-const getFirst = (fullName) => {
-	return fullName.split(' ')[0];
-};
-
-const getFirstName = (fullName) => fullName.split(' ')[0];
-
-console.log(getFirstName(fullName));
-console.log(getFirst(fullName));
+// const fullName = 'Kevin D. Atkins';
+//
+// const getFirst = (fullName) => {
+// 	return fullName.split(' ')[0];
+// };
+//
+// const getFirstName = (fullName) => fullName.split(' ')[0];
+//
+// console.log(getFirstName(fullName));
+// console.log(getFirst(fullName));
